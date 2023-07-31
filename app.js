5873b557d4ef6277656db5f00bec8ef3bfc35541
DESKTOP-ST2H7RM\lenovo <ajikowiyu@gmail.com>
2023-07-31 19:56:27 +0700

    ritual mvc

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log('Hello World')
+const express   = require('express')
+const app       = express()
+const port      = 3000
+
+
+app.use( express.json() )
+app.use( express.urlencoded({extended: false}) )
+
+app.set('views', './view')
+app.set('view engine', 'ejs')
+
+//import file
+const c_beranda = require('./controller/c_beranda')
+
+//route
+app.get('/', c_beranda.index)
+
+
+//jalankan server
+app.listen(port, function() {
+    console.log(
+        `===============================================================\n`+
+        `Aplikasi nodejs anda sudah jalan di http://localhost:${port}\n`+
+        `===============================================================`
+    )
+})
