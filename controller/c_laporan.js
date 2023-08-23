eb26e9eb8ae8f0c7c7e5ceec0566924ff7d6a232
DESKTOP-ST2H7RM\lenovo <ajikowiyu@gmail.com>
2023-08-23 20:35:25 +0700

    laporan stok riwayat: view, pilih barang

--- controller/c_laporan.js
+++ controller/c_laporan.js
@@ -1,9 +1,12 @@
+const m_masterbarang = require('../model/m_masterbarang')
+
 module.exports = {
 
-    stok_riwayat: function(req,res) {
+    stok_riwayat: async function(req,res) {
         let data = {
             req     : req,
             konten  : 'laporan/stok/riwayat',
+            barang  : await m_masterbarang.get_all(),
         }
         res.render('template', data)
     }
